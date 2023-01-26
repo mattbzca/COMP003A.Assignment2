3c4176c7e997672840a68fc73c0806de1f4ee376
matthewtan <matthewtan@LEM231-08>
2023-01-25 20:23:14 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Author: Matthew Tan
+ * Course: COMP-003A
+ * Purpose: Lecture activity for variables, type system, Math, and Console properties
+ * Reference: The C# Player's Guide (4e) by RB Whitaker
+ */
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "COMP-003A - Assignment 2"
+
+        }
+    }
+}
